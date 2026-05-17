12734a5f296864f9cf418d856692d48a9849ece2
NKENG123 <dimitrisavionkeng@gmail.com>
2026-05-17 08:17:02 +0100

    debug: step by step check

--- api/index.php
+++ api/index.php
@@ -1,6 +1,20 @@
 <?php
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+// Test basique d'abord
+echo "PHP fonctionne !<br>";
+echo "PHP version: " . PHP_VERSION . "<br>";
 
+// Vérifie si vendor existe
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    die("<b style='color:red'>ERREUR: vendor/autoload.php manquant - composer install n'a pas tourné</b>");
+}
+echo "vendor OK<br>";
+
+// Vérifie .env
+if (!file_exists(__DIR__ . '/../.env')) {
+    die("<b style='color:orange'>ATTENTION: fichier .env manquant</b>");
+}
+echo ".env OK<br>";
+
+// Lance Laravel
 require __DIR__ . '/../public/index.php';
